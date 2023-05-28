ea0927a24db7c4cddcc4648a32cc19a7f659842d
ats527 <ats@github.com>
2023-05-28 21:23:53 +0530

    changed from local to remote db

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const db = mongoose.connect(process.env.MONGO_URL).then(() => {
+const db = mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.MONGO_PASSWORD}@${process.env.DB_NAME}.9daglqy.mongodb.net/?retryWrites=true&w=majority`).then(() => {
     console.log("Connected to MongoDB");
     connected = true;
 }).catch((err) => {
