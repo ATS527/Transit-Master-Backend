b37201c7f212b9936aa94cfef84d71bbe79a36d6
ats527 <ats@github.com>
2023-07-03 07:17:01 +0530

    hopefully it works now

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -11,11 +11,10 @@ const sendToken = (user, statusCode, res) => {
         expires: new Date(
             Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
         ),
-        // httpOnly: true,
+        httpOnly: true,
         sameSite: "None",
-        secure: false,
+        // secure: true,
         // domain: ".govindsr.me",
-        path: "/"
     };
 
     res.cookie('token', token, options);
